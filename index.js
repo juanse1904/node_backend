64eeb0f17f78f5171b063297462f6d5dafe25200
Sebastian Gomez <gomeztabaresjuansebastian@gmail.com>
2024-01-23 17:03:52 -0500

    more http methods on each route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const routerApi = require('./routes');
 const app = express();
 const port = 5050;
 
+app.use(express.json());
 routerApi(app);
 
 app.listen(port, ()=>{
--- routes/users.router.js
+++ routes/users.router.js
@@ -6,4 +6,22 @@ router.get('/', (req,res)=> {
   res.json(users);
 });
 
+router.post('/', (req,res)=> {
+  const body = req.body;
+  res.status(201).json({
+    message: "created",
+    data: body,
+  });
+});
+
+router.patch('/:id', (req,res)=> {
+  const id = req.params;
+  const body = req.body;
+  res.json({
+    message: "update",
+    data: body,
+    id,
+  });
+});
+
 module.exports = router;
